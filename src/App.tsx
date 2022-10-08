08625b9abf8142aba625434309a9fdfa8b592beb
William Silva <euwilliamsantana@gmail.com>
2022-10-08 11:00:11 -0300

    Initial Structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+export function App() {
+ 
+  return (
+    <div className="bg-[#0D0D0D] h-[200px]">
+
+      <div className="max-w-3xl m-auto">
+
+          
+
+      </div>
+
+    </div>
+  )
+}
