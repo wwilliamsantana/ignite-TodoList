f1d1c47cbaaa3a3429426df7805544bfc07a843e
William Silva <euwilliamsantana@gmail.com>
2022-10-08 17:44:32 -0300

    Logic: Delete, Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,75 @@
 import "./styles/main.css"
 import { PlusCircle } from "phosphor-react"
 
-
 import logoImg from "./assets/logo.svg"
 import { ListEmpty } from "./components/ListEmpty"
 import { Post } from "./components/Posts"
+import { useEffect, useState } from "react"
+import { CheckedState } from "@radix-ui/react-checkbox"
+
+export interface PostProps{
+  id: number
+  title: string
+  isComplete: boolean | CheckedState
+}
+
+
+const posts: PostProps[] = [
+  {
+    id: 1,
+    title: "Finalizar ToDo List",
+    isComplete: false
+  },
+  {
+    id: 2,
+    title: "Subir Projetos no Github",
+    isComplete: false
+  },
+  {
+    id: 3,
+    title: "Estudar matéria de cálculo",
+    isComplete: false
+  }
+]
 
 
 export function App() {
+  const [listPost, setListPost] = useState(posts)
+  const [ newPost, setNewPost] = useState("")
+  const [tasksCountDone, setTasksCountDone] = useState(0)
+
+ 
+  function handleDeletePost(id: number){
+    let newList = listPost.filter(item => item.id !== id)
+
+    setListPost(newList)
+  } 
+
+  function handleCompletePost(state: CheckedState, id: number){
+      let newList = listPost
+
+      newList.map(post => {
+        if(post.id == id){
+          post.isComplete = state
+        }
+      })
+
+      setListPost(newList)
+      setTasksCountDone(() => listPost.filter(item => item.isComplete).length)
+  }
+
+  function handleNewPost(){
+    const newTodo = {
+      id: 4,
+      title: newPost,
+      isComplete: false
+    }
+
+    setListPost(prev => [newTodo, ...prev])
+  }
+
+
+
  
   return (
       <div className="bg-[#0D0D0D] h-[12.5rem]">
@@ -22,10 +84,12 @@ export function App() {
               <input 
               type="text" 
               placeholder="Adicione uma nova tarefa"
-              className="flex-1 rounded-lg bg-[#262626] h-[3.375rem] p-4 placeholder:text-[#808080] border-[#0D0D0D] border-2 outline-none focus:outline-2 focus:outline-[#5E60CE]"
+              onChange={e => setNewPost(e.target.value)}
+              className="flex-1 text-[#f2f2f2] rounded-lg bg-[#262626] h-[3.375rem] p-4 placeholder:text-[#808080] border-[#0D0D0D] border-2 outline-none focus:outline-2 focus:outline-[#5E60CE]"
               />
 
               <button
+                onClick={handleNewPost}
                 className="text-sm font-bold flex items-center p-4 bg-[#1E6F9F] rounded-lg ml-2 text-white gap-2 hover:bg-[#4EA8DE]"
               >
                 Criar
@@ -38,44 +102,34 @@ export function App() {
 
               <div className="flex items-center gap-2">
                 <p className="font-bold text-sm text-[#4EA8DE] leading-4" >Tarefas criadas</p>
-                <span 
-                  className="py-0.5 px-2 rounded-full bg-[#333333] text-xs font-bold leading-4 text-white"
-                >5</span>
+                <span className="py-0.5 px-2 rounded-full bg-[#333333] text-xs font-bold leading-4 text-white">
+                 {listPost.length}
+                </span>
               </div>
 
               <div className="flex items-center gap-2" >
 
-                <p 
-                className="font-bold text-sm text-[#8284fA] leading-4"
-                >
-                  Concluídas
-                </p>
+                <p className="font-bold text-sm text-[#8284fA] leading-4">Concluídas</p>
 
-                <span
-                  className="py-0.5 px-2 rounded-full bg-[#333333] text-xs font-bold leading-4 text-white" 
-                >2 </span>
+                <span className="py-0.5 px-2 rounded-full bg-[#333333] text-xs font-bold leading-4 text-white">
+                {`${tasksCountDone} de ${listPost.length}`} 
+                </span>
               </div>
 
             </div>
              
-              {/* <ListEmpty/> */}
+             
 
               <main className="mt-6">
-                {/* <Post/>
-                <Post/>
-                <Post/>
-                <Post/> */}
-                <ListEmpty/>
+
+                {
+                  listPost.map(item =>  <Post key={item.id} item={item} onDeletePost={handleDeletePost}  onCompletePost={handleCompletePost}/>)
+                }
+                
+                
               </main>
 
-
-              
-
-            
-
           </div>
-
-
       </div>
   )
 }
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,22 +1,45 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { PostProps } from '../App';
 
-export function Post(){
+interface Props {
+  item: PostProps
+  onDeletePost: (id: number) => void
+  onCompletePost: (state: Checkbox.CheckedState, id: number) => void
+}
+
+
+
+export function Post({onDeletePost, onCompletePost,  ...props}: Props){
+  
+   const [checked, setChecked] = useState(false)
+
+   function handleCheckComment(event: Checkbox.CheckedState){
+      onCompletePost(event, props.item.id)
+      setChecked(!checked)
+   }
+
+   function handleDeleteComment(){
+    onDeletePost(props.item.id)
+   }
+
   return (
       <div className="flex bg-[#262626] p-4 gap-3 items-start rounded-lg mb-3">
 
-      <Checkbox.Root className="w-4 h-4 mt-1 rounded-full bg-slate-500">
+      <Checkbox.Root  onCheckedChange={handleCheckComment} className="w-4 h-4  rounded-full bg-slate-500">
         <Checkbox.Indicator >
           <Check className="w-4 h-4  rounded-full text-white bg-violet-500"/>
       </Checkbox.Indicator>
       </Checkbox.Root>
 
       <div className="text-white flex-1 text-sm">
-        <p >Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.Duis vel sed fames integer</p>
+        <p className= {checked ? "line-through text-[#808080]" : ""}>
+          {props.item.title}</p>
       </div>  
       
 
-      <div className="mt-1 text-[#808080] hover:bg-[#333333] hover:text-red-600 hover:rounded">
+      <div onClick={handleDeleteComment} className=" text-[#808080] hover:bg-[#333333] hover:text-red-600 hover:rounded">
         <Trash/>
       </div>
       
